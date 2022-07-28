78ad0423fc8e4cecbb62310ffd13e76098da6b93
MaciejKasperowicz <makasp@wp.pl>
2022-07-28 20:05:43 +0200

    Task 1 completed

--- 01/app.js
+++ 01/app.js
@@ -5,11 +5,33 @@ class App extends React.Component {
     state = {
         counter: 0,
     }
+    constructor(props) {
+        super(props);
+
+        console.log("constructor", props);
+    }
+
     render() {
         console.log('render');
 
-        return <h1>{ this.state.counter }</h1>
+        return <h1>{this.state.counter}</h1>
+    }
+    componentDidMount() {
+        console.log('componentDidMount');
+        this.id = setInterval(() => {
+            const { counter } = this.state;
+            this.setState({ counter: counter + 1 })
+        }, 5000);
+    }
+    componentDidUpdate() {
+        console.log("componentDidUpdate");
+        console.log(this.state);
+        if (this.state.counter === 5) this.componentWillUnmount();
+    }
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+        clearInterval(this.id);
     }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));

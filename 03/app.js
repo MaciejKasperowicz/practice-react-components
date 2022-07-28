59b6c643bb1ebd2339aacaa6bc4c695e2bb1153c
MaciejKasperowicz <makasp@wp.pl>
2022-07-28 21:55:55 +0200

    Task 3 finished

--- 03/app.js
+++ 03/app.js
@@ -3,22 +3,23 @@ import ReactDOM from 'react-dom';
 
 class Article extends React.Component {
     state = {
-        comments: [],
+        comments: ["aa", "aaaa"],
     }
-    
+
     render() {
-        const {title, body} = this.props;
+        const { title, body } = this.props;
+        // const { comments } = this.state;
         return (
             <article>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
+                <h1>{title}</h1>
+                <p>{body}</p>
                 <section>
-                    <form>
+                    <form onSubmit={(e) => this.submitHandler(e)}>
                         <div>
                             <label>
-                                <textarea 
-                                    style={{ "min-width": "300px", "min-height": "120px" }} 
-                                    name="content" 
+                                <textarea
+                                    style={{ "minWidth": "300px", "minHeight": "120px" }}
+                                    name="content"
                                 />
                             </label>
                         </div>
@@ -26,17 +27,39 @@ class Article extends React.Component {
                     </form>
                     <ul>
                         {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {/* {comments.map((comment, i) => <li key={i}>{comment}</li>)} */}
+                        {this.renderComments()}
                     </ul>
                 </section>
             </article>
         )
     }
+
+    renderComments() {
+        const { comments } = this.state;
+        return comments.map((comment, i) => <li key={i}>{comment}</li>)
+    }
+
+    addComment(comment) {
+        this.setState({
+            comments: [...this.state.comments, comment]
+        })
+    }
+
+    submitHandler(e) {
+        e.preventDefault();
+        // console.dir(e.target.elements.content.value);
+        const textArea = e.target.elements[0];
+        const comment = textArea.value;
+        comment && this.addComment(comment);
+        textArea.value = "";
+    }
 }
 
 ReactDOM.render(
-    <Article 
+    <Article
         title="Programowanie jest super!"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
-    />, 
+    />,
     document.querySelector('#root')
 );
